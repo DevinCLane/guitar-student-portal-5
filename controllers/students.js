69e77b022911de984ecf2f07cb3015e666a0f1d9
Devin Lane <devin.c.lane@gmail.com>
2025-02-14 11:34:50 -0800

    student database: add the student's teacher

--- controllers/students.js
+++ controllers/students.js
@@ -1,5 +1,6 @@
 const Student = require("../models/Student");
 const LessonPlan = require("../models/LessonPlan");
+const Teacher = require("../models/Teacher");
 
 module.exports = {
     getStudent: async (req, res) => {
@@ -8,9 +9,11 @@ module.exports = {
             const lessonPlans = await LessonPlan.find({
                 student: req.params.id,
             });
+            const teacher = await Teacher.findById({ _id: req.user.id });
             res.render("student/profile", {
                 student: student,
                 lessonPlans: lessonPlans,
+                teacher: teacher,
             });
         } catch (error) {
             console.log(error);
