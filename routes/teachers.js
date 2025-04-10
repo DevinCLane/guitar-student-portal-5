d2600e75a2f8f21aad6c052e513c8b63529b8e60
Devin Lane <devin.c.lane@gmail.com>
2025-04-10 15:48:42 -0700

    update routes

--- routes/teachers.js
+++ routes/teachers.js
@@ -6,44 +6,72 @@ const { ensureAuth } = require("../middleware/auth");
 const upload = require("../middleware/multer");
 
 /* 
-Dashboard
+Teacher dashboard
 */
+
 // dashboard to view all students
 router.get("/dashboard", ensureAuth, teacherController.getDashboard);
 
 /* 
-Students
+Student Management
 */
-// Display the "add a new student" form
-router.get("/newStudent", ensureAuth, teacherController.getNewStudentForm);
-// create a student
-router.post("/createStudent", ensureAuth, teacherController.createStudent);
+
 // view individual student
 // todo: create view for student
-router.get("/student/:id", ensureAuth, studentController.getStudent);
+router.get("/student/:studentId", ensureAuth, studentController.getStudent);
+// Display add student form
+router.get("/student/new", ensureAuth, teacherController.getNewStudentForm);
+// create new student
+router.post("/student", ensureAuth, teacherController.createStudent);
+// display edit student form
+// todo: create controller method for edit student form
+router.get(
+    "/student/:studentId/edit",
+    ensureAuth,
+    studentController.getEditStudentForm
+);
+// update student
+// todo: create controller method for update student
+router.put("/student/:studentId", ensureAuth, studentController.updateStudent);
+// delete student
+// todo: create controller method for deleting student
+router.delete(
+    "/student/:studentId",
+    ensureAuth,
+    studentController.deleteStudent
+);
 // delete a student
 // todo: create delete student functionality
 router.post("/deleteStudent/:id", ensureAuth, teacherController.deleteStudent);
 
 /* 
-Lesson Plans
+Lesson Management
 */
-// get the student's lesson plans
-router.get(
-    "/student/:studentId/lessonPlans",
-    ensureAuth,
-    // todo: create the controller for getting a student's lesson plans teacher view
-    teacherController.getStudentLessonPlans
-);
 
+// display new lesson form
+router.get(
+    "/student/:studentId/lesson/new",
+    ensureAuth,
+    teacherController.getNewLessonForm
+);
 // create a new lesson plan
 router.post(
-    "/student/:studentId/createLessonPlan",
+    "/student/:studentId/lesson",
+    ensureAuth,
     upload.single("file"),
     // todo: create a new lesson plan as a teacher
-    teacherController.createLessonPlan
+    teacherController.createLesson
 );
-
+router.get(
+    "/student/:studentId/lessonPlan/:lessonId/edit",
+    ensureAuth,
+    teacherController.getEditLessonForm
+);
+router.put(
+    "/student/:studentId/lessonPlan/:lessonId",
+    ensureAuth,
+    teacherController.getEditLessonForm
+);
 // delete a lesson plan
 router.delete(
     "/student/:studentId/lessonPlan/:lessonPlanId",
---
+++ todo.md
@@ -0,0 +1,15 @@
+Dev workflow:
+
+-   Create the controller function
+-   Set up the route
+-   Create the view
+-   Test the feature
+-   Move to the next feature
+
+Workflow:
+
+-   Write all routes first
+-   Create empty controller functions
+-   Create basic view templates
+-   Implement controller logic
+-   Add view details and styling
